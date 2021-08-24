a2f182e126953cfbacb2c3a9fd482198106aa3e3
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2021-08-24 05:34:20 +0100

    refactor code with DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
+  # perform the set_article method for specific actions
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    #Article instance variable
-    @article = Article.find(params[:id])
   end
 
   def index
-    #Articles instance variable
     @articles = Article.all
   end
 
@@ -14,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       #Send success message
       flash[:notice] = 'Article was created successfully.'
@@ -29,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article was updated successfully.'
       redirect_to @article
     else
@@ -39,8 +36,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  # functions below private are only available in the particular controller
+  private
+
+  def set_article
+    #Articles instance variable
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
