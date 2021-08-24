f299b8e26faf9bb45c9c96955aae8d9045057d7f
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2021-08-24 12:54:47 +0100

    added user and articles association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  #Associations
+  has_many :articles
   #Validation
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false }, 
@@ -7,6 +9,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }, 
-                    length: { maximum: 105 }
+                    length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
 end
